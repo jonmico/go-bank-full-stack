dd929b8ac11632b526fd730b098d108e101fb159
Jon Mico <jonmico@gmail.com>
2024-10-04 14:04:57 -0400

    Break out createUsersTable into its own function.

--- server/db/db.go
+++ server/db/db.go
@@ -12,6 +12,7 @@ var DB *sql.DB
 func InitDB() {
 	var err error
 
+	fmt.Println("Opening database connection...")
 	DB, err = sql.Open("sqlite3", "go-bank.db")
 
 	if err != nil {
@@ -19,22 +20,32 @@ func InitDB() {
 		panic("could not connect to database")
 	}
 
+	fmt.Println("Successfully connected to database.")
+
 	createTables()
 }
 
 func createTables() {
-	createUsersTable := `
-		CREATE TABLE IF NOT EXISTS users (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			email TEXT NOT NULL UNIQUE,
-			password TEXT NOT NULL
-		)
-	`
-
-	_, err := DB.Exec(createUsersTable)
+	err := createUsersTable()
 
 	if err != nil {
 		fmt.Println(err)
-		panic("could not create users table")
 	}
+}
+
+func createUsersTable() error {
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)
+`
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		return err
+	}
+
+	return err
 }
