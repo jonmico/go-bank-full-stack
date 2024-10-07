59e4df7a5153953bfe34e36d7aaadaf20d967875
Jon Mico <jonmico@gmail.com>
2024-10-07 10:18:23 -0400

    Home loader works. Not necessarily something we want to do, but it works.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AppLayout from './app-layout';
 import Home from './pages/home';
 
+interface User {
+  userId: number;
+  email: string;
+}
+
+async function homeLoader() {
+  const res = await fetch('http://localhost:3000');
+  const data: { message: string; users: User[] } = await res.json();
+
+  return data;
+}
+
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -9,6 +21,7 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+        loader: homeLoader,
       },
     ],
   },
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,21 @@
+import { useLoaderData } from 'react-router-dom';
+
+interface User {
+  userId: number;
+  email: string;
+}
+
 export default function Home() {
+  // FIXME: How do we type this data?
+  const data: { message: string; users: User[] } = useLoaderData();
+  console.log(data.message, data.users);
+
   return (
     <div>
       <h2>This is the home page.</h2>
+      {data.users.map((u) => (
+        <div key={u.userId}>{u.email}</div>
+      ))}
     </div>
   );
 }
