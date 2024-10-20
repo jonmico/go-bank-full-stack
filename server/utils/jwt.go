4a55ad11a705fe5dbc3d86bf2feb0028c81d1db1
Jon Mico <jonmico@gmail.com>
2024-10-19 22:47:04 -0400

    Add GenerateToken for jwt implementation.

---
+++ server/utils/jwt.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+)
+
+func GenerateToken(email string, userId int64) (string, error) {
+	err := godotenv.Load()
+
+	if err != nil {
+		return "", err
+	}
+
+	secretKey := os.Getenv("JWT_SECRET")
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour * 2).Unix(),
+	})
+
+	return token.SignedString(secretKey)
+}
