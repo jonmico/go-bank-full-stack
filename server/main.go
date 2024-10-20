e0d0c7e8b37bf383bafe901fa6ad6f147ecb6258
Jon Mico <jonmico@gmail.com>
2024-10-20 12:56:05 -0400

    Add VerifyToken function.

--- server/main.go
+++ server/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"example.com/go-bank-server/db"
 	"example.com/go-bank-server/routes"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
+
 	db.InitDB()
 	server := gin.Default()
 
--- server/utils/jwt.go
+++ server/utils/jwt.go
@@ -1,20 +1,14 @@
 package utils
 
 import (
+	"errors"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/joho/godotenv"
 )
 
 func GenerateToken(email string, userId int64) (string, error) {
-	err := godotenv.Load()
-
-	if err != nil {
-		return "", err
-	}
-
 	secretKey := os.Getenv("JWT_SECRET")
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -24,4 +18,38 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(tokenString string) (int64, error) {
+	secretKey := os.Getenv("JWT_SECRET")
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return 0, errors.New("could not parse token")
+	}
+
+	isTokenValid := token.Valid
+
+	if !isTokenValid {
+		return 0, errors.New("invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return 0, errors.New("invalid token claims")
+	}
+
+	userId := int64(claims["userId"].(float64))
+
+	return userId, nil
 }
